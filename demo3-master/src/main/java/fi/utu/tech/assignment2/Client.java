480124f8e588502e3cd7bad8f1e66ca31ccd908b
WherBanan <121439318+WherBanan@users.noreply.github.com>
2023-11-13 18:02:02 +0000

    Demo3 t.2

--- demo3-master/src/main/java/fi/utu/tech/assignment2/Client.java
+++ demo3-master/src/main/java/fi/utu/tech/assignment2/Client.java
@@ -1,9 +1,28 @@
 package fi.utu.tech.assignment2;
 
-public class Client {
+import java.io.IOException;
+import java.net.Socket;
+import java.io.OutputStream;
 
+public class Client {
+    public static final int PORT = 20540;
     public static void main(String[] args) {
-        // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
+        String serverAddress = "localhost";
+        try (Socket socket = new Socket(serverAddress, PORT)){
+            System.out.println("Connected to server");
+
+            // Send message to server
+            String message = "Van tuu trii";
+            OutputStream os = socket.getOutputStream();
+            os.write(message.getBytes());
+            
+
+
+        
+            System.out.println("Connection closed");
+        } catch (IOException e) {
+            System.err.println(e);
+        }
     }
 
 }
--- demo3-master/src/main/java/fi/utu/tech/assignment2/Server.java
+++ demo3-master/src/main/java/fi/utu/tech/assignment2/Server.java
@@ -1,9 +1,33 @@
 package fi.utu.tech.assignment2;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-
+    public static final int PORT = 20540;
     public static void main(String[] args) {
-        // TODO: Kopioi edellisen tehtäväsi vastaus tähän pohjalle
-    }
+        try (ServerSocket server = new ServerSocket(PORT)) {
+            System.out.println("Wait for client connection");
+            Socket client = server.accept();
+            System.out.println(
+                "Connection from" + client.getInetAddress() + " port " + client.getPort());
 
+            // Read message from client
+            InputStream is = client.getInputStream();
+            byte[] buffer = new byte[1024];
+            int bytesRead = is.read(buffer);
+
+            if (bytesRead > 0) {
+                String message = new String(buffer, 0, bytesRead);
+                System.out.println("Message got: " + message);
+            }
+
+            client.close();
+            server.close();
+        } catch (IOException e) {
+            System.err.println(e);
+        }
+    }
 }
